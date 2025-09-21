86ef7b31e555cea860eb15f7f4d295e01bb02672
Kyaw Htet <xsquadster@gmail.com>
2025-09-21 08:35:46 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/components/app/sales/sales-form.tsx
+++ src/components/app/sales/sales-form.tsx
@@ -8,6 +8,7 @@ import * as z from "zod";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
