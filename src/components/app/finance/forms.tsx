f83b2c75e7d0385041a46ea4cf2b37c3b45a9244
Kyaw Htet <xsquadster@gmail.com>
2025-10-03 11:06:30 +0000

    does not work. Is there any other calendar like in sales and purchase?

--- src/components/app/finance/forms.tsx
+++ src/components/app/finance/forms.tsx
@@ -110,7 +110,9 @@ export function AddExpenseForm({ onSave, onSuccess, categories }: FormProps<Omit
                     mode="single"
                     selected={field.value}
                     onSelect={field.onChange}
-                    disabled={(date) => date > new Date()}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
                     initialFocus
                   />
                 </PopoverContent>
