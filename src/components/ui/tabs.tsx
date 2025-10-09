98e8226e65bd38499904b4a44dc6ef8380e78809
Kyaw Htet <xsquadster@gmail.com>
2025-10-09 03:38:10 +0000

    make the app fit to all devices browsers such as mobile phone, tablets a

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -15,7 +15,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-auto items-center justify-center gap-2 flex-wrap",
+      "inline-flex h-auto items-center justify-start gap-2 flex-wrap",
       className
     )}
     {...props}
