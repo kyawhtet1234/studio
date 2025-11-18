46f87d0f10678f1f3186df1d68eb66f0b503414a
Kyaw Htet <xsquadster@gmail.com>
2025-11-18 10:19:24 +0000

    I did not see any pop up receipt for printing

--- src/components/app/sales/sales-form.tsx
+++ src/components/app/sales/sales-form.tsx
@@ -1,4 +1,5 @@
 
+
 "use client";
 
 import { useState, useEffect } from "react";
@@ -677,3 +678,4 @@ export function SalesForm({ stores, customers, onSave, onAddCustomer }: SalesFor
     
 
     
+
